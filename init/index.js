89beca49a5771bb507a3c87943627e97b643e407
Bhalu Muduli <mudulibhalu@gmail.com>
2025-12-01 00:34:04 +0530

    Update init/index.js

--- init/index.js
+++ init/index.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;  // ✔ Use Atlas, not localhost
 
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log("Connected to Atlas DB");
   })
   .catch((err) => {
     console.log(err);
@@ -18,9 +18,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner: "6924a854adf56298bb1c5a28"}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6924a854adf56298bb1c5a28"
+  }));
   await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+  console.log("Database seeded successfully!");
 };
 
 initDB();
